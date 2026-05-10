ef658a479263e54b08180e3e30de77187c640aa7
Amir Khazeev <amirhazeev60189@gmail.com>
2026-05-10 23:10:02 +0300

    Updated app icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,6 +28,8 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
+            isShrinkResources = false
+
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
             )
